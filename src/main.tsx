24efbe2654e73f22b46a6d76e5af68a4c69e6bfe
Teru <218044131+daveteru@users.noreply.github.com>
2026-03-11 15:30:27 +0700

    feat:add register page

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,17 @@ import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import './index.css'
 import Home from './pages/Home';
+import Register from './pages/Register';
 
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home/>,
+  },
+  {
+    path: "/register",
+    element: <Register/>,
   },
 ]);
 
