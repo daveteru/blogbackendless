a456638fc32505e0ccc6cc1dbed921ad9d3e9088
Teru <218044131+daveteru@users.noreply.github.com>
2026-03-11 15:50:23 +0700

    feat:add login feature page (#2)

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { RouterProvider } from "react-router/dom";
 import './index.css'
 import Home from './pages/Home';
 import Register from './pages/Register';
+import Login from './pages/Login';
 
 
 const router = createBrowserRouter([
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
   {
     path: "/register",
     element: <Register/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
   },
 ]);
 
